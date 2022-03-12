a383979d474c3b07d84d395c9c729a742c90e0aa
TheKeineAhnung <fireball73@gmx.de>
2022-03-12 16:16:36 +0100

    🐛 fixed closeSettingsFrame function

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -1,9 +1,19 @@
 export function closeSettingsFrame() {
-  if (document.querySelector("#iframe")) {
-    document.querySelector("#iframe").contentWindow.document.body.innerHTML =
-      "";
-    document.querySelector("#iframe").contentWindow.document.head.innerHTML =
-      "";
+  let iframe = document.querySelector("#iframe");
+  iframe.remove();
+  let frame = document.createElement("iframe");
+  frame.id = "iframe";
+  frame.classList.add("panel");
+  frame.setAttribute("data-hj-allow-iframe", "");
+  let mainarea = document.querySelector("#mainarea");
+  mainarea.style.overflow = "unset";
+  mainarea.appendChild(frame);
+  // frameVisible is a variable of rettungssimulator.online
+  // You can find it in the controlCenter.js file
+  for (const i in frameVisible) {
+    frameVisible[i] = true;
   }
-  closeFrame();
+  // hidePanels is a function of rettungssimulator.online
+  // You can find it in the controlCenter.js file
+  hidePanels();
 }
