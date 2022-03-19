48a16d2e2b62c2e044562ff2fe5c9aa3d94e1e09
TheKeineAhnung <fireball73@gmx.de>
2022-03-19 21:38:47 +0100

    ✨ switched static urls to dynamic urls

--- src/js/credits.js
+++ src/js/credits.js
@@ -2,12 +2,16 @@ import * as j from "jquery";
 
 async function getCredits() {
   let scriptInfo;
-  await j.getJSON(
-    "https://keineahnung.eu/resi-script-manager/json/credits.json",
-    (data) => {
-      scriptInfo = data;
-    }
-  );
+  let url;
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "development") {
+    url = "https://localhost/json/credits.json";
+  } else {
+    url = "https://keineahnung.eu/resi-script-manager/json/credits.json";
+  }
+  await j.getJSON(url, (data) => {
+    scriptInfo = data;
+  });
   return scriptInfo;
 }
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,25 +2,31 @@ import * as j from "jquery";
 
 async function getScriptInfo() {
   let scriptInfo;
-  await j.getJSON(
-    "https://keineahnung.eu/resi-script-manager/json/info.json",
-    (data) => {
-      scriptInfo = data;
-    }
-  );
+  let url;
+  if (process.env.NODE_ENV === "development") {
+    url = "https://localhost/json/info.json";
+  } else {
+    url = "https://keineahnung.eu/resi-script-manager/json/info.json";
+  }
+  await j.getJSON(url, (data) => {
+    scriptInfo = data;
+  });
   return scriptInfo;
 }
 
 async function getScriptNames() {
   let scriptNames = new Array();
-  await j.getJSON(
-    "https://keineahnung.eu/resi-script-manager/json/info.json",
-    (data) => {
-      for (let i in data) {
-        scriptNames.push(data[i].name);
-      }
+  let url;
+  if (process.env.NODE_ENV === "development") {
+    url = "https://localhost/json/info.json";
+  } else {
+    url = "https://keineahnung.eu/resi-script-manager/json/info.json";
+  }
+  await j.getJSON(url, (data) => {
+    for (let i in data) {
+      scriptNames.push(data[i].name);
     }
-  );
+  });
   return scriptNames;
 }
 
