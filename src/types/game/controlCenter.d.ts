f030c7ef6033f4d52ddf3b90125a26acd1778314
TheKeineAhnung <fireball73@gmx.de>
2022-07-16 22:17:19 +0200

    ✨ added controlCenter types

---
+++ src/types/game/controlCenter.d.ts
@@ -0,0 +1,11 @@
+declare let frameVisible: {
+  map: boolean;
+  ad: boolean;
+  calls: boolean;
+  missions: boolean;
+  chat: boolean;
+  radio: boolean;
+  departments: boolean;
+};
+
+declare function hidePanels(): void;
