42b0da31a1d749a7eae143c98a6af58b4cd5c665
TheKeineAhnung <fireball73@gmx.de>
2022-07-17 13:46:52 +0200

    ✨ added typeDefinition for const FRAMES and function openFrame

--- src/types/game/controlCenter.d.ts
+++ src/types/game/controlCenter.d.ts
@@ -1,3 +1,26 @@
+declare const FRAMES: {
+  '1/1/4/5': {
+    grid: '1 / 1 / 4 / 5';
+    hide: ['chat', 'calls', 'missions', 'departments', 'radio', 'map', 'ad'];
+  };
+  '1/1/4/4': {
+    grid: '1 / 1 / 4 / 4';
+    hide: ['chat', 'calls', 'missions', 'map', 'ad', 'radio'];
+  };
+  '1/2/4/5': {
+    grid: '1 / 2 / 4 / 5';
+    hide: ['chat', 'calls', 'missions', 'departments', 'radio'];
+  };
+  '1/3/4/5': {
+    grid: '1 / 3 / 4 / 5';
+    hide: ['departments', 'missions', 'radio'];
+  };
+  '1/2/4/4': {
+    grid: '1 / 2 / 4 / 4';
+    hide: ['missions', 'radio', 'chat', 'calls'];
+  };
+};
+
 declare let frameVisible: {
   map: boolean;
   ad: boolean;
@@ -9,3 +32,5 @@ declare let frameVisible: {
 };
 
 declare function hidePanels(): void;
+
+declare function openFrame(url: string, element: string): void;
