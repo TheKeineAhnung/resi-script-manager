d5807698098fffa0275b926973c7750e614dcc67
TheKeineAhnung <fireball73@gmx.de>
2022-08-26 22:46:35 +0200

    ✨ added customMapZoom script

---
+++ src/scripts/customMapZoom/customMapZoom.ts
@@ -0,0 +1,63 @@
+import { variableIsNull } from '../../ts/errors/console';
+
+const customMapZoom = async function (): Promise<any> {
+  const focusMap = function () {
+    console.log('TRIGGER!');
+    const northernCoordinate = localStorage.getItem('customMapZoomNorth');
+    const easternCoordinate = localStorage.getItem('customMapZoomEast');
+    const southernCoordinate = localStorage.getItem('customMapZoomSouth');
+    const westernCoordinate = localStorage.getItem('customMapZoomWest');
+    if (northernCoordinate === null) {
+      variableIsNull(Object.keys({ northernCoordinate })[0], 'customMapZoom');
+      return;
+    }
+    if (easternCoordinate === null) {
+      variableIsNull(Object.keys({ easternCoordinate })[0], 'customMapZoom');
+      return;
+    }
+    if (southernCoordinate === null) {
+      variableIsNull(Object.keys({ southernCoordinate })[0], 'customMapZoom');
+      return;
+    }
+    if (westernCoordinate === null) {
+      variableIsNull(Object.keys({ westernCoordinate })[0], 'customMapZoom');
+      return;
+    }
+    const parsedNorthernCoordinate = parseFloat(northernCoordinate);
+    const parsedEasternCoordinate = parseFloat(easternCoordinate);
+    const parsedSouthernCoordinate = parseFloat(southernCoordinate);
+    const parsedWesternCoordinate = parseFloat(westernCoordinate);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-error
+    const northEast = L.latLng(
+      parsedNorthernCoordinate,
+      parsedEasternCoordinate
+    );
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-error
+    const southWest = L.latLng(
+      parsedSouthernCoordinate,
+      parsedWesternCoordinate
+    );
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-error
+    mymap.fitBounds(L.latLngBounds(southWest, northEast), {
+      padding: [20, 20],
+      maxZoom: 14
+    });
+  };
+  focusMap();
+  const zoomButton: HTMLAnchorElement | null =
+    document.querySelector('a#mapCenter');
+
+  if (zoomButton === null) {
+    variableIsNull(Object.keys({ zoomButton })[0], 'customMapZoom');
+    return;
+  }
+
+  zoomButton.addEventListener('click', focusMap);
+};
+
+export { customMapZoom };
