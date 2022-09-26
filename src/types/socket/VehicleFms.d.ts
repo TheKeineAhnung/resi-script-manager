5f5c73fe13f38da5ce358dcf43e7ab0c1b46804d
TheKeineAhnung <fireball73@gmx.de>
2022-09-26 18:21:31 +0200

    🏷️ added mission interface parameters

--- src/types/socket/VehicleFms.d.ts
+++ src/types/socket/VehicleFms.d.ts
@@ -1,9 +1,14 @@
 interface VehicleFms {
+  canOvertakePatient: boolean;
+  countPersonal: number;
+  fms5Type: 'radio' | 'patient' | 'carrier' | null;
   userVehicleID: number;
   userVehicleName: string;
   userVehicleFMS: number;
+  userVehicleFakeFMS: number;
   userMissionID: number;
   userDepartmentID: number;
+  userName: string;
   countPersonal: number;
   vehicleShortName: string;
   vehicleID: number;
