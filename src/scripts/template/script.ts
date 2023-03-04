0a1c471f7a068ba47a3e4b8c635cb336b7cdb7b8
TheKeineAhnung <fireball73@gmx.de>
2023-03-04 14:22:25 +0100

    ✨ added API import in template script

--- src/scripts/template/script.ts
+++ src/scripts/template/script.ts
@@ -4,6 +4,8 @@ import {
   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   variableIsUndefined
 } from '../../ts/errors/console';
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+import { apiGet, apiPost } from '../../ts/helper/api';
 
 const scriptName = async function (): Promise<any> {
   // Enter your script here
