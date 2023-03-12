04376f00daad484fd86b78c6904c4f0318104206
TheKeineAhnung <fireball73@gmx.de>
2023-03-12 14:52:32 +0100

    ✨ added api stuff

--- src/ts/helper/general.ts
+++ src/ts/helper/general.ts
@@ -2,4 +2,8 @@ const sleep = function (ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-export { sleep };
+const firstLetterUppercase = function (string: string): string {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export { firstLetterUppercase, sleep };
